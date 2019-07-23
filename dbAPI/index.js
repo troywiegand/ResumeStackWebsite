8494606e163825c1f1972d19852d36e16d29bd7c
Troy Wiegand <troy@troyiegand.com>
2019-07-22 22:14:58 -0400

    Fix Port issue

--- dbAPI/index.js
+++ dbAPI/index.js
@@ -5,7 +5,7 @@ const troylocale = './troy.db'
 const ashleylocale = './ashley.db'
 
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://localhost:33507"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Origin", "http://localhost:3001"); // update to match the domain you will make the request from
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
   });
